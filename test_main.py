4154592ac8484ecedaed02db9c552a2e94d3b284
Muhammed Ali <muhamzyali@gmail.com>
2022-02-19 15:10:25 +0100

    correct test run

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 from main import app
 
-def test_hello():
+def test_main():
     response = app.test_client().get('/') # Creates a test client for this application.
 
     assert response.status_code == 200 # assert the stutus code of the page('/') is 200
-    assert response.data == b'Welcome to my Flask A pp' # assert the return statement ton the page
+    assert response.data == b'Welcome to my Flask App' # assert the return statement ton the page
