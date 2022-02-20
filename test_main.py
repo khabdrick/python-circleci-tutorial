88fd1a14aa95dc2fba4c440657d77906263a86c3
Muhammed Ali <muhamzyali@gmail.com>
2022-02-20 21:31:47 +0100

    v2

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ from main import app
 def test_main():
     response = app.test_client().get('/') # Creates a test client for this application.
 
-    assert response.status_code == 200 # assert the stutus code of the page('/') is 200
-    assert response.data == b'Welcome to my Flask App' # assert the return statement ton the page
+    assert response.status_code == 400 # assert the stutus code of the page('/') is 200
+    assert response.data == b'Flask App' # assert the return statement ton the page
